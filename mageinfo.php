0d8fb953dcc31103dd39d01d4e0465ce293ef30d
Ryan Belisle <ryan@ryebell.me>
2015-11-24 20:21:01 -0500

    add mageinfo file

---
+++ mageinfo.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once ("./app/Mage.php");
+Mage::app();
+$version = Mage::getVersion();
+$base_url = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB);
+$modules = array_keys((array)Mage::getConfig()->getNode('modules')->children());
+
+echo "<b>Version:</b> <br>" . $version . "<br>";
+echo "<b>Base URL:</b> <br>" . $base_url . "<br>";
+echo "<b>Installed Modules:</b> <br>";
+foreach ($modules as $module) {
+    echo $module . ", ";
+}
+
+?>
