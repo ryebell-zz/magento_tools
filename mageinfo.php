95f458ff4d34aee93bee87083e773745970db375
Ryan Belisle <ryan@ryebell.me>
2015-11-25 15:03:26 -0500

    add module status to list

--- mageinfo.php
+++ mageinfo.php
@@ -4,13 +4,15 @@ require_once ("./app/Mage.php");
 Mage::app();
 $version = Mage::getVersion();
 $base_url = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB);
-$modules = array_keys((array)Mage::getConfig()->getNode('modules')->children());
+$modules = (array)Mage::getConfig()->getNode('modules')->children();
 
 echo "<b>Version:</b> <br>" . $version . "<br>";
 echo "<b>Base URL:</b> <br>" . $base_url . "<br>";
 echo "<b>Installed Modules:</b> <br>";
-foreach ($modules as $module) {
-    echo $module . ", ";
+foreach ($modules as $module => $moduleSettings) {
+    echo $module . ' [' 
+    . ($moduleSettings->is('active') ? 'active' : 'disabled') . "] <br>";
 }
 
 ?>
+
